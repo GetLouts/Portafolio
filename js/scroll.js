efb9d05d0e5e586e47b17b594dbd6121f6af7d8e
Alejandro Rangel <87172222+GetLouts@users.noreply.github.com>
2023-05-21 16:18:03 +0200

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+// let ubicacionPrincipal = window.pageYOffset;
+// let $nav = document.querySelector('#nav')
+
+// window.addEventListener('scroll', function() {
+//     let ubicacionActual = window.pageYOffset
+
+//     if(ubicacionPrincipal >= ubicacionActual ){
+//         $nav.style.top = '0'
+//     } else {
+//         $nav.style.top = '-100px'
+//     }
+
+//     ubicacionPrincipal = ubicacionActual
+// })
